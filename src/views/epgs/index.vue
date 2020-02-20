546f9649b7f8e9895054f13dfa273edb05130d96
zhaodan <zdailwh@sina.com>
2020-02-20 18:28:20 +0800

    loading改为加载中

--- src/views/epgs/index.vue
+++ src/views/epgs/index.vue
@@ -53,7 +53,7 @@
     <el-table
       v-loading="listLoading"
       :data="list"
-      element-loading-text="Loading"
+      element-loading-text="加载中"
       fit
       highlight-current-row
       size="small"
